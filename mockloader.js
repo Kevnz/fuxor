ded9598754cd794b87cbb6dc0f98301a8c59ea44
Kevin Isom <kevin.isom@gmail.com>
2015-05-20 12:09:19 +1200

    added exporting add and remove

--- mockloader.js
+++ mockloader.js
@@ -4,31 +4,25 @@ var Module = require('module');
 
 var _defaultLoad = Module._load;
 
-var fs = require('fs');
+var mappings =  {};
 
-var options = {};
-if(process.env.NODE_ENV == 'test') {
-    options = JSON.parse(fs.readFileSync('./test/container.json', {encoding:'utf8'}));
-    console.log(options);
-} else {
- try {
-   options = fs.readFileSync('./container.json', {encoding:'utf8'});
- } catch (err) {
-     console.log(__dirname);
-     console.log(process.env)
- }
-}
 Module._load  = function(moduleName) {
-        console.log("lol at you");
-        console.log("no " + moduleName + " for you");
-        console.log('WELL ACTUALLY');
-        console.log(options)
         if (moduleName.indexOf('.') === 0) {
             //file, only di node modules
             return _defaultLoad(moduleName);
         }
-        if (options.modules[moduleName]) {
-            return {};
+        if (mappings[moduleName]) {
+            return mappings[moduleName];
         }
         return _defaultLoad(moduleName);
+}
+
+module.exports = { 
+    add: function (mapping) {
+        mappings[mapping.name] = mapping.result;
+    },
+    remove: function (name) {
+        mappings[name] = null;
+        delete mappings[name];
+    }
 }
