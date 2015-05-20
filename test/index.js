6ac0fbeb306a9a1b8a9fd343f44ddfe4c28fa0b5
Kevin Isom <kevin.isom@gmail.com>
2015-05-20 12:09:25 +1200

    testing

--- test/index.js
+++ test/index.js
@@ -4,10 +4,58 @@ var test = require("tap").test;
 test("Requiring overrides", function (t) { 
 	
 	var fs = require('fs');
-	console.log('???')
-	require('../mockloader');
-	var fs2 = require('fs');
-	//console.log(fs2);
-	//t.notEqual(fs , fs2, "should not be the same");
+	var mocking = require('../mockloader');
+	mocking.add({name:'fs', result: {
+		test: function (result) {
+			t.ok(result, 'this should be called' );
+			t.end();
+		}
+	}})
+	var fs2 = require('fs'); 
+	t.notEqual(fs , fs2, "should not be the same");
+	fs2.test(true);
+	 
+});
+
+test("Requiring overrides multiple times", function (t) { 
+	
+	var fs = require('fs');
+	var mocking = require('../mockloader');
+	mocking.add({name:'fs', result: {
+		test: function (result) {
+			t.ok(result, 'this should be called' );
+			
+		}
+	}});
+	mocking.add({name:'mocker', result: {
+		test: function (result) {
+			t.ok(result, 'this should be called as well' );
+			t.end();
+		}
+	}});
+	var fs2 = require('fs');  
+	fs2.test(true);
+	var mock = require('mocker');
+	mock.test(true);
+	 
+});
+
+test("Removing a module", function (t) { 
+	
+	var mocking = require('../mockloader');
+	mocking.add({name:'fs', result: {
+		mockFunction: function (result) {
+			t.ok(result, 'this should be called' );
+			
+		}
+	}});
+	var fsMock = require('fs');  
+	fsMock.mockFunction(true);
+	mocking.remove('fs');
+	
+	var realFs = require('fs');
+	
+	t.ok(realFs.mockFunction === undefined);
 	t.end();
+	 
 });
