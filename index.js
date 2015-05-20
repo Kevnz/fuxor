2e86ac2cf963eb44681aa7a57806eab35e14bb77
Kevin Isom <kevin.isom@gmail.com>
2015-05-20 12:27:08 +1200

    main entry point

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 'use strict';
-//console.log(module);
-require("./mockloader");
+
+var Module = require('module');
+
+var _defaultLoad = Module._load;
+
+var mappings =  {};
+
+Module._load  = function(moduleName) {
+        if (moduleName.indexOf('.') === 0) {
+            //file, only di node modules
+            return _defaultLoad(moduleName);
+        }
+        if (mappings[moduleName]) {
+            return mappings[moduleName];
+        }
+        return _defaultLoad(moduleName);
+}
+
+module.exports = {
+    add: function (mapping) {
+        mappings[mapping.name] = mapping.result;
+    },
+    remove: function (name) {
+        mappings[name] = null;
+        delete mappings[name];
+    }
+}
