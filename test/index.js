e75a8f3c73675ac8c3701c3f2899fd9aad6ab737
Kevin Isom <kevinisom@Shenandoah.fritz.box>
2016-11-27 20:53:33 +1300

    more tests

--- test/index.js
+++ test/index.js
@@ -1,62 +1,142 @@
-var test = require("tap").test;
+const test = require('tap').test;
 
-var mocking = require('../index');
-	
-test("Requiring overrides", function (t) { 
-	mocking.clear();
-	var fs = require('fs');
+const fuxor = require('../index');
 
-	mocking.add({name:'fs', result: {
-		test: function (result) {
-			t.ok(result, 'this should be called' );
-			t.end();
-		}
-	}})
-	var fs2 = require('fs'); 
-	t.notEqual(fs , fs2, "should not be the same");
-	fs2.test(true);
-	
+test('Requiring overrides', function (t) {
+  fuxor.clear();
+  const fs = require('fs');
+
+  fuxor.add({name:'fs', result: {
+    test: function (result) {
+      t.ok(result, 'this should be called' );
+      t.end();
+    }
+  }})
+  const fs2 = require('fs');
+  t.notEqual(fs , fs2, 'should not be the same');
+  fs2.test(true);
+
 });
 
-test("Requiring overrides multiple times", function (t) { 
-	mocking.clear();
-	var fs = require('fs');
-	 
-	mocking.add({name:'fs', result: {
-		test: function (result) {
-			t.ok(result, 'this should be called' );
-			
-		}
-	}});
-	mocking.add({name:'mocker', result: {
-		test: function (result) {
-			t.ok(result, 'this should be called as well' );
-			t.end();
-		}
-	}});
-	var fs2 = require('fs');  
-	fs2.test(true);
-	var mock = require('mocker');
-	mock.test(true);
-	 
+test('Requiring overrides multiple times', function (t) {
+  fuxor.clear();
+  const fs = require('fs');
+
+  fuxor.add({name:'fs', result: {
+    test: function (result) {
+      t.ok(result, 'this should be called' );
+
+    }
+  }});
+  fuxor.add({name:'mocker', result: {
+    test: function (result) {
+      t.ok(result, 'this should be called as well' );
+      t.end();
+    }
+  }});
+  const fs2 = require('fs');
+  fs2.test(true);
+  const mock = require('mocker');
+  mock.test(true);
+
 });
 
-test("Removing a module", function (t) { 
-	
-	mocking.clear();
-	mocking.add({name:'fs', result: {
-		mockFunction: function (result) {
-			t.ok(result, 'this should be called' );
-			
-		}
-	}});
-	var fsMock = require('fs');  
-	fsMock.mockFunction(true);
-	mocking.remove('fs');
-	
-	var realFs = require('fs');
-	
-	t.ok(realFs.mockFunction === undefined);
-	t.end();
-	 
+test('Removing a module', function (t) {
+  fuxor.clear();
+  fuxor.add({name:'fs', result: {
+    mockFunction: function (result) {
+      t.ok(result, 'this should be called' );
+    }
+  }});
+  const fsMock = require('fs');
+  fsMock.mockFunction(true);
+  fuxor.remove('fs');
+  const realFs = require('fs');
+  t.ok(realFs.mockFunction === undefined);
+  t.end();
+});
+
+test('Clearing all modules', function (t) {
+  fuxor.clear();
+  fuxor.add({ name:'fs', result: {
+    mockFunction: function (result) {
+      t.ok(result, 'this should be called' );
+    }
+  }});
+  fuxor.add({ name:'util', result: {
+    mockedFunction: function (result) {
+      t.ok(result, 'this should be called' );
+    }
+  }});
+  const fsMock = require('fs');
+  fsMock.mockFunction(true);
+  const utilMock = require('util');
+  utilMock.mockedFunction(true);
+  fuxor.clear();
+  const realFs = require('fs');
+  const realUtil = require('util');
+  t.ok(realFs.mockFunction === undefined);
+  t.ok(realUtil.mockedFunction === undefined);
+  t.end();
+});
+
+test('Only override modules that have been loaded', function (t) {
+  fuxor.clear();
+  fuxor.add({ name:'not-really-a-module', result: {
+    mockFunction: function (result) {
+      t.ok(result, 'this should be called' );
+    }
+  }});
+  const fs = require('fs');
+  t.ok(fs.mockFunction === undefined);
+  t.ok(typeof fs.readFileSync === 'function');
+  t.end();
+
+});
+
+test('Override local modules as well', function (t) {
+  fuxor.clear();
+  fuxor.add({ name:'./mock', result: {
+    mockFunction: function () {
+      t.ok(true, 'this should be called' );
+      t.end();
+    }
+  }});
+  const filemock = require('./mock');
+  filemock.mockFunction();
+
+});
+
+test('Local module not added should be called', function (t) {
+  fuxor.clear();
+  fuxor.add({ name:'./mock', result: {
+    mockFunction: function () {
+      t.ok(true, 'this should be called' );
+    }
+  }});
+  const nomock = require('./no-mock');
+  nomock(()=> {
+    t.ok(true, 'this should be called' );
+    t.end();
+  });
+
+});
+
+test('Adding multiple overrides in one add', function (t) {
+  fuxor.clear();
+  fuxor.add([{ name:'fs', result: {
+    test: function (result) {
+      t.ok(result, 'this should be called' );
+    }
+  }}, {
+    name:'nothing', result: function (result) {
+      t.ok(result, 'this should be called' );
+    }
+  }
+  ])
+  const fs = require('fs');
+  const nothing = require('nothing');
+  fs.test(true);
+  nothing(true);
+  t.end();
 });
