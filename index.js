e166e18f182c227e39cb5820313f45eaa70ce2ef
Kevin Isom <kevin.isom@gmail.com>
2017-01-16 20:55:29 +1300

    add namespace functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Module = require('module');
 
 const defaultLoad = Module._load;
 const mappings =  new Map();
+const namespaceMappings = new Map();
+const path = require('path');
 
 Module._load = function(moduleName, module) {
   if (moduleName.indexOf('.') === 0 && !mappings.has(moduleName)) {
@@ -9,6 +11,15 @@ Module._load = function(moduleName, module) {
     const parsedPath = path.parse(module.filename);
     const filePath = path.resolve(process.cwd(), parsedPath.dir, moduleName);
     return defaultLoad(filePath);
+  }
+  if (moduleName.indexOf('::') > -1) {
+    console.log('from the _load cwd', process.cwd());
+    console.log('from the _load __dirname', __dirname);
+    const [ namespace, moduleToLoad ] = moduleName.split('::');
+    console.log('namespace', namespace);
+    const modulePath = path.join(process.cwd(), namespaceMappings.get(namespace), moduleToLoad);
+    console.log('modulePath', modulePath);
+    return defaultLoad(modulePath);
   }
   if (mappings.has(moduleName)) {
     return mappings.get(moduleName);
@@ -34,5 +45,8 @@ module.exports = {
   },
   reset: function () {
     Module._load = defaultLoad;
+  },
+  namespace: function(name, pathStart) {
+    namespaceMappings.set(name, pathStart);
   }
 }
